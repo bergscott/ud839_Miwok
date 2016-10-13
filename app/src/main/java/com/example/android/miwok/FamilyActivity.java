ea27e89a47ff7dbbe5f7ab429936ab2f7432dab4
bergscott <berg.scott@ymail.com>
2016-10-13 12:07:25 -0500

    make the category activitiees release the MediaPlayer resources upon completion

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -52,6 +52,9 @@ public class FamilyActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                // release MediaPlayer resources to prepare for the initialization of the new player
+                releaseMediaPlayer();
+
                 // get audioResourceId of clicked Word
                 int audioResourceId = words.get(position).getAudioResourceId();
 
@@ -60,8 +63,32 @@ public class FamilyActivity extends AppCompatActivity {
 
                 // start playback of the audio
                 mMediaPlayer.start();
+
+                // set onCompletionListener to release the MediaPlayer resources upon completion
+                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        releaseMediaPlayer();
+                    }
+                });
             }
         });
+    }
 
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
